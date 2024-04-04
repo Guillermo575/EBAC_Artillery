a83b1b54f0ef5232a32b97b6d1560e13fc57cec3
Guillermo575 <30479672+Guillermo575@users.noreply.github.com>
2024-04-03 22:34:11 -0600

    ajustes tarea

--- Artillery/Assets/_Scripts/Bala.cs
+++ Artillery/Assets/_Scripts/Bala.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Bala : MonoBehaviour
 {
+    #region Variables
+    private GameManager gameManager;
+    #endregion
+
+    #region Start & Update
     void Start()
     {
+        gameManager = GameManager.SingletonGameManager;
     }
     void Update()
     {
+        if (this.gameObject.transform.position.y < gameManager.NivelSuelo)
+        {
+            StartCoroutine(CoroutineBallDeath());
+        }
     }
+    #endregion
+
+    #region Collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Solido")
+        {
+            StartCoroutine(CoroutineBallDeath());
+        }
+    }
+    #endregion
+
+    #region Ball Destroy
+    IEnumerator CoroutineBallDeath()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        Destroy(this.gameObject);
+    }
+    #endregion
 }
--- Artillery/Assets/_Scripts/Canon.cs
+++ Artillery/Assets/_Scripts/Canon.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Canon : MonoBehaviour
 {
+    #region Variables
     [SerializeField] private GameObject BalaPrefab;
     private GameObject puntaCanon;
     private float rotacion;
+    private GameManager gameManager;
+    #endregion
+
+    #region Start & Update
     void Start()
     {
+        gameManager = GameManager.SingletonGameManager;
         puntaCanon = transform.Find("PuntaCanon").gameObject;
     }
     void Update()
     {
-        rotacion += Input.GetAxis("Horizontal") * GameManager.VelocidadRotacion;
+        rotacion += Input.GetAxis("Horizontal") * gameManager.VelocidadRotacion;
         if (rotacion <= 90 && rotacion >= 0)
         {
             transform.eulerAngles = new Vector3(rotacion, 90, 0.0f);
@@ -21,11 +27,16 @@ public class Canon : MonoBehaviour
         if (rotacion < 0) rotacion = 0;
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
-            Rigidbody tempRB = temp.GetComponent<Rigidbody>();
-            Vector3 direccionDisparo = transform.rotation.eulerAngles;
-            direccionDisparo.y = 90 - direccionDisparo.x;
-            tempRB.velocity = direccionDisparo.normalized * GameManager.VelocidadBala;
+            if (gameManager.DisparosPorJuego > 0)
+            {
+                GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+                Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+                Vector3 direccionDisparo = transform.rotation.eulerAngles;
+                direccionDisparo.y = 90 - direccionDisparo.x;
+                tempRB.velocity = direccionDisparo.normalized * gameManager.VelocidadBala;
+                gameManager.DisparosPorJuego--;
+            }
         }
     }
+    #endregion
 }
