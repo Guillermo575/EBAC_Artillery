14652edafcbae7638fff137579a6b5ff4bcf007a
Guillermo575 <30479672+Guillermo575@users.noreply.github.com>
2024-05-24 23:50:04 -0600

    HUD AJUSTES

--- Artillery/Assets/_Scripts/HUD/HUDIconoBalas.cs
+++ Artillery/Assets/_Scripts/HUD/HUDIconoBalas.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using TMPro;
 public class HUDIconoBalas : MonoBehaviour
 {
     private Canon jugador;
+    public TMP_Text txtLabel;
     public List<Sprite> lstImages;
     void Start()
     {
@@ -16,5 +18,6 @@ public class HUDIconoBalas : MonoBehaviour
         var nombre = "HUD_Bala_" + jugador.GetBalaPrefab().Nombre;
         var lst = (from x in lstImages where x.name == nombre select x).ToList();
         this.gameObject.GetComponent<Image>().sprite = lst.First();
+        if (txtLabel != null) txtLabel.text = jugador.GetBalaPrefab().Nombre;
     }
 }
